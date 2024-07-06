7b79bb9f38814a4189eb49f478173a8126ec6214
Salima Ammar <selimaammar07@gmail.com>
2024-07-06 16:18:01 -0400

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api"; // Ensure this matches your backend port
+const API_URL = process.env.REACT_APP_BACKEND_URL; // Ensure this matches your backend port
 
 export const registerUser = (userData) => {
-  return axios.post(`${API_URL}/users/register`, userData);
+  return axios.post(`${API_URL}/api/users/register`, userData);
 };
 
 export const loginUser = (userData) => {
-  return axios.post(`${API_URL}/users/login`, userData);
+  return axios.post(`${API_URL}/api/users/login`, userData);
 };
 
 export const getFoodItems = () => {
-  return axios.get(`${API_URL}/food`);
+  return axios.get(`${API_URL}/api/food`);
 };
 
 export const addFoodItem = (foodData) => {
-  return axios.post(`${API_URL}/food`, foodData);
+  return axios.post(`${API_URL}/api/food`, foodData);
 };
